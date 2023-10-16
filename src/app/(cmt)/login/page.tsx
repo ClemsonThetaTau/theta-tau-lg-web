d2c4a6cfefef93c6aaef5d67ebf375f7bf0912c5
Alex <korte@clemson.edu>
2023-10-15 20:12:32 -0400

    Update Authentication

--- src/app/(cmt)/login/page.tsx
+++ src/app/(cmt)/login/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { UserAuthForm } from "@/components/sections/auth/user-auth-form"
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+import { UserAuthForm } from '@/components/sections/auth/user-auth-form'
+import { GiGearHammer } from 'react-icons/gi'
 
 export default function AuthenticationPage() {
   return (
@@ -25,70 +26,46 @@ export default function AuthenticationPage() {
         />
       </div>
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
+        {/* <Link
           href="/examples/authentication"
           className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            buttonVariants({ variant: 'ghost' }),
+            'absolute right-4 top-4 md:right-8 md:top-8'
           )}
         >
           Login
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        </Link> */}
+        <div className="relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
+            <GiGearHammer />
+            &nbsp;Theta Tau ΛΓ
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;Whatsoever thy hand findeth to do, do it with thy might.&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Ecclesiastes 9:10</footer>
             </blockquote>
           </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
+              <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your personal email below to login
               </p>
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
               <Link
-                href="/terms"
+                href="/forgot-password"
                 className="underline underline-offset-4 hover:text-primary"
               >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
+                Forgot your password?
               </Link>
-              .
             </p>
           </div>
         </div>
--- src/components/sections/auth/user-auth-form.tsx
+++ src/components/sections/auth/user-auth-form.tsx
@@ -1,13 +1,16 @@
-"use client"
+'use client'
 
-import * as React from "react"
+import * as React from 'react'
+import Link from 'next/link'
 
-import { cn } from "@/lib/utils"
-import { BiLogoGithub } from "react-icons/bi"
-import { ImSpinner3 } from "react-icons/im"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { db } from '@/firebase/config'
+import { doc, getDoc } from 'firebase/firestore'
+
+import { cn } from '@/lib/utils'
+import { ImSpinner3 } from 'react-icons/im'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -24,10 +27,15 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   }
 
   return (
-    <div className={cn("grid gap-6", className)} {...props}>
+    <div className={cn('grid gap-6', className)} {...props}>
       <form onSubmit={onSubmit}>
         <div className="grid gap-2">
-          <div className="grid gap-1">
+          <div className="grid gap-1 mb-4">
+          <div className="relative flex text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Email
+                </span>
+            </div>
             <Label className="sr-only" htmlFor="email">
               Email
             </Label>
@@ -40,33 +48,30 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               autoCorrect="off"
               disabled={isLoading}
             />
+              <div className="relative flex text-xs uppercase mt-2">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Password
+                </span>
+            </div>
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder=""
+              type="password"
+              autoCapitalize="none"
+              autoComplete="current-password"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
           </div>
           <Button disabled={isLoading}>
-            {isLoading && (
-              <ImSpinner3 className="mr-2 h-4 w-4 animate-spin" />
-            )}
+            {isLoading && <ImSpinner3 className="mr-2 h-4 w-4 animate-spin" />}
             Sign In with Email
           </Button>
         </div>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
-        {isLoading ? (
-          <ImSpinner3 className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <BiLogoGithub className="mr-2 h-4 w-4" />
-        )}{" "}
-        Github
-      </Button>
     </div>
   )
 }
