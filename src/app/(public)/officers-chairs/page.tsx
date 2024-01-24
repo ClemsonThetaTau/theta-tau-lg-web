32ac7ba8a66d9892b58b88186eaf9cbbb3c82cf5
Alex <alex.k.korte@gmail.com>
2024-01-24 02:22:26 -0500

    Setup Frontend Position Display

--- src/app/(public)/officers-chairs/page.tsx
+++ src/app/(public)/officers-chairs/page.tsx
@@ -1,7 +1,17 @@
 import React from "react"
 
+import OfficerGrid from "./officerGrid"
+
 export default function OfficersChairs() {
     return (
-        <></>
+        <div>
+             <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight mt-10 text-center">
+                Executive Committee
+            </h2>
+            <OfficerGrid />
+            <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight mt-0 text-center">
+                The Chairs
+            </h2>
+        </div>
     )
 }
