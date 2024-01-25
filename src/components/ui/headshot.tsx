d794c8556a597a5feed21a0f9dc88bbbaf488986
Alex <alex.k.korte@gmail.com>
2024-01-24 21:46:34 -0500

    Add Skeletons + Fix Headshot Bugs

--- src/components/ui/headshot.tsx
+++ src/components/ui/headshot.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { Brother } from '@/components/types/brother'
+import { PublicBrother } from '@/components/types/brother'
+import { Skeleton } from '@/components/ui/skeleton'
 
 interface HeadshotProps {
-  brother: Brother
+  brother: PublicBrother
 }
 
 const Headshot: React.FC<HeadshotProps> = ({ brother }) => {
   return (
-    <div className="w-48" key={brother.email}>
+    <div className="w-48" key={brother.displayEmail}>
       <img
-        src={brother.image}
-        alt={brother.name}
+        src={brother.profilePicture}
+        alt={`${brother.firstName} ${brother.lastName}`}
         className="w-full h-auto object-cover aspect-square"
       />
       <div>
-        <h2 className="text-lg">{brother.name}</h2>
-        <p className="text-sm">{brother.email}</p>
+        <h2 className="text-lg">{`${brother.firstName} ${brother.lastName}`}</h2>
+        <p className="text-sm">{brother.displayEmail}</p>
         <p className="text-sm">{brother.major}</p>
       </div>
     </div>
@@ -31,19 +32,31 @@ const PositionHeadshot: React.FC<PositionHeadshotProps> = ({
   position,
 }) => {
   return (
-    <div className="w-48" key={brother.email}>
+    <div className="w-48" key={brother.displayEmail}>
       <img
-        src={brother.image}
-        alt={brother.name}
+        src={brother.profilePicture}
+        alt={`${brother.firstName} ${brother.lastName}`}
         className="w-full h-auto object-cover aspect-square"
       />
       <div>
         <h2 className="text-lg">{position}</h2>
-        <p className="text-sm">{brother.name}</p>
-        <p className="text-sm">{brother.email}</p>
+        <p className="text-sm">{`${brother.firstName} ${brother.lastName}`}</p>
+        <p className="text-sm">{brother.displayEmail}</p>
       </div>
     </div>
   )
 }
 
-export { Headshot, PositionHeadshot }
+const SkeletonHeadshot: React.FC = () => {
+  return (
+    <div className="w-48">
+      <Skeleton className="aspect-square" />
+      <div className="space-y-2 mt-4">
+        <Skeleton className="h-4 w-48" />
+        <Skeleton className="h-4 w-32" />
+      </div>
+    </div>
+  )
+}
+
+export { Headshot, PositionHeadshot, SkeletonHeadshot }
