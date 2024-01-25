2dfdc9d6053de526c89cb9fc29ef62b2537a58a3
Alex <alex.k.korte@gmail.com>
2024-01-24 21:46:56 -0500

    Add Skeletons to Brother Grid

--- src/app/(public)/brothers/brotherGrid.tsx
+++ src/app/(public)/brothers/brotherGrid.tsx
@@ -1,29 +1,24 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Brother, PublicBrother, PublicData } from '@/components/types/brother'
-import { Headshot } from '@/components/ui/headshot'
+import { PublicBrother, PublicBrotherData } from '@/components/types/brother'
+import { Headshot, SkeletonHeadshot } from '@/components/ui/headshot'
 
 import { db } from '@/firebase/firebase'
 import { doc, getDoc } from 'firebase/firestore'
 
 export default function BrotherGrid() {
-  const [brothers, setBrothers] = useState<Brother[]>([])
+  const [brothers, setBrothers] = useState<PublicBrother[]>()
 
   useEffect(() => {
     // Fetch brothers data from database
     const fetchData = async () => {
       const brothersDoc = doc(db, 'public', 'brothers')
       const brothersSnapshot = await getDoc(brothersDoc)
-      const brothersData: PublicData = brothersSnapshot.data() as PublicData
+      const brothersData: PublicBrotherData = brothersSnapshot.data() as PublicBrotherData
       const brothersList = brothersData.displayOrder.map((userId: any) => {
-        const data = brothersData.brotherList[userId]
-        const brother: Brother = {
-          name: `${data.firstName} ${data.lastName}`,
-          email: data.displayEmail,
-          image: data.profilePicture,
-          major: data.major,
-        }
+        const brother = brothersData.brotherList[userId] as PublicBrother
+
         return brother
       })
       setBrothers(brothersList)
@@ -38,9 +33,25 @@ export default function BrotherGrid() {
         Meet the Brothers of the Lambda Gamma Chapter!
       </h1>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-5 justify-items-center items-center">
-        {brothers.map((brother, idx) => (
+        {(brothers && brothers.map((brother, idx) => (
           <Headshot key={idx} brother={brother} />
-        ))}
+        ))) || (
+          <>
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+            <SkeletonHeadshot />
+          </>
+        )}
       </div>
     </section>
   )
