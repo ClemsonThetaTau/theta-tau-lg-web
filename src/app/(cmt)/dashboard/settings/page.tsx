5a79f7b4a6a5ea3d413bbe3d65c739e18a5ceb3f
Alex <korte@clemson.edu>
2024-06-13 21:40:00 -0700

    Add Skeleton Form to Settings

--- src/app/(cmt)/dashboard/settings/page.tsx
+++ src/app/(cmt)/dashboard/settings/page.tsx
@@ -1,8 +1,50 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { auth } from '@/firebase/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import { db, storage } from '@/firebase/firebase'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
+
 import { Separator } from "@/components/ui/separator"
 import { ProfilePicture } from "./profile-picture"
-import { ProfileForm } from "./profile-form"
+import { ProfileForm, ProfileFormValues } from "./profile-form"
+import { SkeletonForm } from '@/components/ui/skeleton-form'
 
 export default function SettingsProfilePage() {
+  const { push } = useRouter()
+
+  const [profilePicture, setProfilePicture] = useState<string>()
+  const [defaultValues, setDefaultValues] = useState<Partial<ProfileFormValues>>()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid
+        const userData = await getDoc(doc(db, 'users', uid))
+
+        if (userData.exists()) {
+          console.log('Document data:', userData.data())
+          const data = userData.data()
+
+          setProfilePicture(data.profilePicture)
+          const defaultValues: Partial<ProfileFormValues> = {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            displayEmail: data.displayEmail,
+          }
+
+          setDefaultValues(defaultValues)
+        }
+      } else {
+        push('/login')
+      }
+    })
+  }, [])
+
   return (
     <div className="space-y-6">
       <div>
@@ -12,7 +54,7 @@ export default function SettingsProfilePage() {
         </p>
       </div>
       <Separator />
-      <ProfileForm />
+      {profilePicture && defaultValues && <ProfileForm defaultValues={defaultValues} profilePicture={profilePicture} /> || <SkeletonForm />}
     </div>
   )
 }
