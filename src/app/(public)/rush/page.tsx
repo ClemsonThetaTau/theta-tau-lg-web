67653c7a2895a0d7d82b69a63da61bcefa96b541
Alex <korte@clemson.edu>
2024-08-30 12:33:31 -0400

    Add speed dating to events

--- src/app/(public)/rush/page.tsx
+++ src/app/(public)/rush/page.tsx
@@ -2,11 +2,12 @@ import { EventSection } from './event-section'
 
 export default function Rush() {
   var offset = -300; //Timezone offset for EST in minutes.
-  const targetDate = new Date('2024-08-29T23:00:00Z')
+  const targetDate = new Date('2024-08-30T23:00:00Z')
 
   return (
     <div className="container mx-auto p-4">
-        <EventSection title={"Info Session"} description='Join us for an informative session about rush week!' date={targetDate} image='/images/rush/info-session.png' location={{name: "Brackett 100", latitude: 34.67888266242274, longitude: -82.83690615210095}} backgroundColor={"#FFEAC5"} />
+        {/* <EventSection title={"Info Session"} description='Join us for an informative session about rush week!' date={targetDate} image='/images/rush/info-session.png' location={{name: "Brackett 100", latitude: 34.67888266242274, longitude: -82.83690615210095}} backgroundColor={"#FFEAC5"} /> */}
+        <EventSection title={"Speed Dating"} description='Come get to know our members!' date={targetDate} image='/images/rush/speed-dating.png' location={{name: "Hendrix 121", latitude: 34.67609869625334, longitude: -82.83186138196058}} backgroundColor={"#FFEAC5"} />
     </div>
   )
 }
