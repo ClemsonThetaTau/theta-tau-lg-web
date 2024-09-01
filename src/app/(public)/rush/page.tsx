6ee6f1b9b7b270b2a30a4ff73e5faa282e4afe7e
Alex <alex.k.korte@gmail.com>
2024-08-31 23:57:53 -0400

    Update page.tsx

--- src/app/(public)/rush/page.tsx
+++ src/app/(public)/rush/page.tsx
@@ -2,7 +2,7 @@ import { EventSection } from './event-section'
 
 export default function Rush() {
   var offset = -300; //Timezone offset for EST in minutes.
-  const targetDate = new Date('2024-08-31T18:00:00Z')
+  const targetDate = new Date('2024-09-01T18:00:00Z')
 
   return (
     <div className="container mx-auto p-4">
