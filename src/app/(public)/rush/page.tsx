1eda497cc8e62068b8b9e6b3d0a79c66a8ba094c
Alex <korte@clemson.edu>
2024-09-03 18:26:15 -0400

    Add comm serve event

--- src/app/(public)/rush/page.tsx
+++ src/app/(public)/rush/page.tsx
@@ -7,7 +7,8 @@ export default function Rush() {
   return (
     <div className="container mx-auto p-4">
         {/* <EventSection title={"Info Session"} description='Join us for an informative session about rush week!' date={targetDate} image='/images/rush/info-session.png' location={{name: "Brackett 100", latitude: 34.67888266242274, longitude: -82.83690615210095}} backgroundColor={"#FFEAC5"} /> */}
-        <EventSection title={"Labor Day Cookout"} description='Meet us at Y-Beach for some hamburgers and games!' date={targetDate} image='/images/rush/cookout.png' location={{name: "Y-Beach", latitude: 34.682091461464395, longitude: -82.86103865227058}} backgroundColor={"#FFEAC5"} />
+        {/* <EventSection title={"Labor Day Cookout"} description='Meet us at Y-Beach for some hamburgers and games!' date={targetDate} image='/images/rush/cookout.png' location={{name: "Y-Beach", latitude: 34.682091461464395, longitude: -82.86103865227058}} backgroundColor={"#FFEAC5"} /> */}
+        <EventSection title={"Community Service"} description='Join us for a day of community service!' date={targetDate} image='/images/rush/community-service.png' location={{name: "Hendrix Meeting Rooms", latitude: 34.67608092642256, longitude: -82.83185037456722}} backgroundColor={"#FFEAC5"} />
     </div>
   )
 }
